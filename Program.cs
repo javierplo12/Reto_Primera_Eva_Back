d28f5781e727eaa212b2fa23c7cb29b0505ddef8
a25612 <a25612@svalero.com>
2024-11-19 11:52:47 +0100

    Añadimos politicas del cors para que no de error los fetch

--- Program.cs
+++ Program.cs
@@ -3,13 +3,24 @@ using Reto_Primera_Eva.Controllers;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Habilitar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Inicializacion de datos
+// Inicialización de datos
 PeliculaController.InicializarDatos();
 ButacaController.InicializarButacas();
 SalaController.InicializarSalas();
@@ -17,14 +28,17 @@ EntradaController.InicializarDatos();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline
+// if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+// Aplicar la política de CORS
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
