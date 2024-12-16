f1b432b00aa85d2d93da64f1838778df71648f19
a25612 <a25612@svalero.com>
2024-12-16 13:12:17 +0100

    Actualizamos pedido.cs para guardar los datos del cliente

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -1,62 +1,62 @@
-using Microsoft.AspNetCore.Mvc;
-using Models;
-using System.Collections.Generic;
-using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using System.Collections.Generic;
+    using System.Linq;
 
-namespace Reto_Primera_Eva.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PedidoController : ControllerBase
+    namespace Reto_Primera_Eva.Controllers
     {
-        private static List<Pedido> pedidos = new List<Pedido>();
-
-        // GET para obtener los pedidos
-        [HttpGet]
-        public ActionResult<IEnumerable<Pedido>> GetPagos()
+        [Route("api/[controller]")]
+        [ApiController]
+        public class PedidoController : ControllerBase
         {
-            return Ok(pedidos);
-        }
+            private static List<Pedido> pedidos = new List<Pedido>();
 
-        // GET para obtener los pedidos por id
-        [HttpGet("{id}")]
-        public ActionResult<Pedido> GetPagoById(int id)
-        {
-            var pago = pedidos.FirstOrDefault(p => p.Id == id);
-            if (pago == null)
+            // GET para obtener los pedidos
+            [HttpGet]
+            public ActionResult<IEnumerable<Pedido>> GetPagos()
             {
-                return NotFound(new { message = $"El pago con ID {id} no existe." });
-            }
-            return Ok(pago);
-        }
-
-        // Metodo para añadir un nuevo pedido
-        [HttpPost]
-        public ActionResult<Pedido> CreatePago([FromBody] Pedido nuevoPago)
-        {
-            // Asignar un ID único a cada pedido
-            nuevoPago.Id = pedidos.Count > 0 ? pedidos.Max(p => p.Id) + 1 : 1;
-
-            // Agregar el nuevo pago
-            pedidos.Add(nuevoPago);
-
-            // Devolver el pedido creado con su nuevo ID
-            return CreatedAtAction(nameof(GetPagoById), new { id = nuevoPago.Id }, nuevoPago);
-        }
-
-        // Elimina una función existente por ID
-        [HttpDelete("{id}")]
-        public ActionResult DeletePedido(int id)
-        {
-            var pedidoExistente = pedidos.FirstOrDefault(f => f.Id == id);
-
-            if (pedidoExistente == null)
-            {
-                return NotFound("Pedido no encontrado.");
+                return Ok(pedidos);
             }
 
-            pedidos.Remove(pedidoExistente);
-            return NoContent();
+            // GET para obtener los pedidos por id
+            [HttpGet("{id}")]
+            public ActionResult<Pedido> GetPagoById(int id)
+            {
+                var pago = pedidos.FirstOrDefault(p => p.Id == id);
+                if (pago == null)
+                {
+                    return NotFound(new { message = $"El pago con ID {id} no existe." });
+                }
+                return Ok(pago);
+            }
+
+            // Metodo para añadir un nuevo pedido
+            [HttpPost]
+            public ActionResult<Pedido> CreatePago([FromBody] Pedido nuevoPago)
+            {
+                // Asignar un ID único a cada pedido
+                nuevoPago.Id = pedidos.Count > 0 ? pedidos.Max(p => p.Id) + 1 : 1;
+
+                // Agregar el nuevo pago
+                pedidos.Add(nuevoPago);
+
+                // Devolver el pedido creado con su nuevo ID
+                return CreatedAtAction(nameof(GetPagoById), new { id = nuevoPago.Id }, nuevoPago);
+            }
+
+            // Elimina una función existente por ID
+            [HttpDelete("{id}")]
+            public ActionResult DeletePedido(int id)
+            {
+                var pedidoExistente = pedidos.FirstOrDefault(f => f.Id == id);
+
+                if (pedidoExistente == null)
+                {
+                    return NotFound("Pedido no encontrado.");
+                }
+
+                pedidos.Remove(pedidoExistente);
+                return NoContent();
+            }
         }
     }
-}
--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -3,15 +3,21 @@ namespace Models
     public class Pedido
 {
     public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Correo { get; set; }
+    public string Telefono { get; set; }
     public string Pelicula { get; set; }
     public string Dia { get; set; }
     public string Hora { get; set; }
     public string ButacasSeleccionadas { get; set; }
     public decimal Precio { get; set; }
 
-    public Pedido(int id, string pelicula, string dia, string hora, string butacasSeleccionadas, decimal precio)
+    public Pedido(int id, string nombre, string correo, string telefono, string pelicula, string dia, string hora, string butacasSeleccionadas, decimal precio)
     {
         Id = id;
+        Nombre = nombre;
+        Correo = correo;
+        Telefono = telefono;
         Pelicula = pelicula;
         Dia = dia;
         Hora = hora;
